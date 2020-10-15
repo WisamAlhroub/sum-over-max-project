5a1b5c3c66c8ae25a77a4be9b8349f2493afcdef
WisamAlhroub <63550902+WisamAlhroub@users.noreply.github.com>
2020-10-15 17:16:21 +0300

    Implemented the ListSumService functionality with test configuration

--- src/main/java/org/testteam/services/ListSumService.java
+++ src/main/java/org/testteam/services/ListSumService.java
@@ -1,4 +1,30 @@
 package org.testteam.services;
 
+import java.util.List;
+
 public class ListSumService {
+    private List<Integer> numbers;
+    private static ListSumService sumService = null;
+
+    private ListSumService(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static ListSumService getInstance(List<Integer> numbers) {
+        return sumService = new ListSumService(numbers);
+    }
+
+    public Integer[] getNumbers() {
+        return numbers.toArray(Integer[]::new);
+    }
+
+    public void setNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (Integer number : numbers) sum += number;
+        return sum;
+    }
 }
--- src/test/java/org/testteam/services/ListSumServiceTest.java
+++ src/test/java/org/testteam/services/ListSumServiceTest.java
@@ -1,7 +1,30 @@
 package org.testteam.services;
 
+import org.junit.Test;
+import java.util.List;
 import static org.junit.Assert.*;
 
 public class ListSumServiceTest {
+
+    @Test
+    public void testInitialization(){
+        Integer[] set = new Integer[]{1, 3, 5, 2};
+        ListSumService sumService = ListSumService.getInstance(List.of(set));
+        assertEquals(set, sumService.getNumbers());
+    }
+
+    @Test
+    public void testSumMethod() {
+        Integer[] set = new Integer[]{1, 3, 5, 2};
+        ListSumService sumService = ListSumService.getInstance(List.of(set));
+        assertEquals(11, sumService.sum());
+    }
+
+    @Test
+    public void testSumMethodNegative() {
+        Integer[] set = new Integer[]{-1, -3, -5, -2};
+        ListSumService sumService = ListSumService.getInstance(List.of(set));
+        assertEquals(-11, sumService.sum());
+    }
 
 }
