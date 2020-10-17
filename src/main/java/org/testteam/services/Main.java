4d85711d2a17fc8852a6c55121890de0744403f7
WisamAlhroub <63550902+WisamAlhroub@users.noreply.github.com>
2020-10-17 23:08:43 +0300

    Added main

---
+++ src/main/java/org/testteam/services/Main.java
@@ -0,0 +1,14 @@
+package org.testteam.services;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        FileWriter fileWriter = new FileWriter(new File("filename.txt"));
+        fileWriter.write("Hello, world");
+        fileWriter.close();
+    }
+}
