9589edfffe46d07bdad01ea6fbe2f170782135e5
WisamAlhroub <63550902+WisamAlhroub@users.noreply.github.com>
2020-10-18 00:57:55 +0300

    yaaay

--- src/main/java/org/testteam/services/Main.java
+++ src/main/java/org/testteam/services/Main.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class MainClass {
+public class Main {
     public static void main(String[] args) throws IOException {
 
         FileWriter fileWriter = new FileWriter(new File("filename.txt"));
